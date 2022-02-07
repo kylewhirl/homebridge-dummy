d8154a4582d629635696360c1b6851c6bd78abed
Kyle Worrall <65330257+kylewhirl@users.noreply.github.com>
2022-02-06 18:47:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 "use strict";
 
 var Service, Characteristic, HomebridgeAPI;
-const { HomebridgeDummyVersion } = require('./package.json');
+var applescript = require('applescript');
+const { HomebridgeShortcutsVersion } = require('./package.json');
 
 module.exports = function(homebridge) {
 
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   HomebridgeAPI = homebridge;
-  homebridge.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
+  homebridge.registerAccessory("homebridge-shortcuts", "Siri Shorcuts", Shortcuts);
 }
 
 
-function DummySwitch(log, config) {
+function Shortcuts(log, config) {
   this.log = log;
   this.name = config.name;
+  this.onCommand = "tell application ''Shortcuts Events'' to set theResult to run shortcut " + "''" + config['on'] + "''";
+  this.offCommand = "tell application ''Shortcuts Events'' to set theResult to run shortcut " + "''" + config['off'] + "''";
   this.stateful = config.stateful;
-  this.reverse = config.reverse;
-  this.time = config.time ? config.time : 1000;		
-  this.resettable = config.resettable;
+  this.time = config.time ? config.time : 1000;
   this.timer = null;
   this._service = new Service.Switch(this.name);
   
   this.informationService = new Service.AccessoryInformation();
   this.informationService
       .setCharacteristic(Characteristic.Manufacturer, 'Homebridge')
-      .setCharacteristic(Characteristic.Model, 'Dummy Switch')
-      .setCharacteristic(Characteristic.FirmwareRevision, HomebridgeDummyVersion)
-      .setCharacteristic(Characteristic.SerialNumber, 'Dummy-' + this.name.replace(/\s/g, '-'));
+      .setCharacteristic(Characteristic.Model, 'Shortcuts')
+      .setCharacteristic(Characteristic.FirmwareRevision, HomebridgeShortcutsVersion)
+      .setCharacteristic(Characteristic.SerialNumber, 'Shortcuts-' + this.name.replace(/\s/g, '-'));
   
   this.cacheDirectory = HomebridgeAPI.user.persistPath();
   this.storage = require('node-persist');
@@ -36,7 +37,6 @@ function DummySwitch(log, config) {
   this._service.getCharacteristic(Characteristic.On)
     .on('set', this._setOn.bind(this));
 
-  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
 
   if (this.stateful) {
 	var cachedState = this.storage.getItemSync(this.name);
@@ -48,25 +48,38 @@ function DummySwitch(log, config) {
   }
 }
 
-DummySwitch.prototype.getServices = function() {
+Shortcuts.prototype.getServices = function() {
   return [this.informationService, this._service];
 }
 
-DummySwitch.prototype._setOn = function(on, callback) {
+Shortcuts.prototype.setState = function(powerOn, callback) {
+	var accessory = this;
+	var state = powerOn ? 'on' : 'off';
+	var prop = state + 'Command';
+	var command = accessory[prop].replace(/''/g, '"');
+
+	applescript.execString(command, done);
+
+	function done(err, rtn) {
+		if (err) {
+			accessory.log('Error: ' + err);
+			callback(err || new Error('Error setting ' + accessory.name + ' to ' + state));
+		} else {
+			accessory.log('Set ' + accessory.name + ' to ' + state);
+			callback(null);
+		}
+	}
+}
+
+Shortcuts.prototype._setOn = function(on, callback) {
 
   this.log("Setting switch to " + on);
 
-  if (on && !this.reverse && !this.stateful) {
-    if (this.resettable) {
-      clearTimeout(this.timer);
-    }
+  if (on && !this.stateful) {
     this.timer = setTimeout(function() {
       this._service.setCharacteristic(Characteristic.On, false);
     }.bind(this), this.time);
-  } else if (!on && this.reverse && !this.stateful) {
-    if (this.resettable) {
-      clearTimeout(this.timer);
-    }
+  } else if (!on  && !this.stateful) {
     this.timer = setTimeout(function() {
       this._service.setCharacteristic(Characteristic.On, true);
     }.bind(this), this.time);
